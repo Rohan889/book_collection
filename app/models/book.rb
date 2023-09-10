4b4ba9ca72dc042191afe026c7998117446a709a
Rohan889 <rohan8.talluri@gmail.com>
2023-09-09 21:32:38 -0500

    test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
     #validates :title, presence: true
+    #test
     validates :author, presence: true
     #Making sure that that the objects has price and author 
     validates :price, numericality: { greater_than_or_equal_to: 0 }
