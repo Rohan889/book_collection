de835f29fd855b41011622e4b4dcb02045969505
Rohan889 <rohan8.talluri@gmail.com>
2023-09-09 22:18:26 -0500

    testing to see if the hosting works

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
     #validates :title, presence: true
     #test
+    #Testing to see if the hosting works properly
     validates :author, presence: true
     #Making sure that that the objects has price and author 
     validates :price, numericality: { greater_than_or_equal_to: 0 }
